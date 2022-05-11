e1a37bf931b4235cc9caa34f680103b9bd750cba
unknown <jeferson.narvaez.dev@gmail.com>
2022-05-11 14:07:57 -0500

    Design of app an basic selection of pokemon

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container-pokemon">
+    <img class="hidden-pokemon" :src="imgSrc" alt="pokemon" />
+    <img v-if="reveal" class="fade-in" :src="imgSrc" alt="pokemon" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    reveal: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  computed: {
+    imgSrc() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+    },
+  },
+};
+</script>
+<style scoped>
+/* Pokemon Picture */
+.container-pokemon {
+  height: 200px;
+}
+
+img {
+  position: absolute;
+  height: 200px;
+  left: 50%;
+  -webkit-transform: translateX(-50%);
+  transform: translateX(-50%);
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+
+.hidden-pokemon {
+  filter: brightness(0);
+}
+</style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,6 +1,17 @@
 <template>
   <h1>¿Quien es este pokémon?</h1>
+  <PokemonPicture :pokemonId="3" :reveal="false" />
+  <PokemonOptions />
 </template>
 <script>
-export default {};
+import PokemonPicture from "@/components/PokemonPicture.vue";
+import PokemonOptions from "@/components/PokemonOptions.vue";
+import getPokemonOptions from "@/helpers/getPokemonOptins";
+getPokemonOptions();
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+};
 </script>
